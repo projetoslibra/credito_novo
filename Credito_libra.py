8bd4e7bf95ed95b82a960392e9be5ff44b81e1b3
Juan Carneiro <juan.carneiro@libracapital.com.br>
2025-10-22 11:18:19 -0300

    second commit

--- Credito_libra.py
+++ Credito_libra.py
@@ -204,12 +204,12 @@ USERS = {
     "jose":     {"senha": "Jose33",     "tipo": "comercial", "agente": "Jose"},
     "sayonara": {"senha": "Sayonara33", "tipo": "comercial", "agente": "Sayonara"},
     "joao":     {"senha": "Joao33",     "tipo": "comercial", "agente": "Joao"},
-    "breno":    {"senha": "Breno13",    "tipo": "comercial", "agente": "Breno"},
+    "Andressa":    {"senha": "Andressa33",    "tipo": "comercial", "agente": "Breno"},
     # === ANALISTAS ===
     "leonardo": {"senha": "Leonardo13", "tipo": "analista", "agente": None},
     "rafael":   {"senha": "Rafael13",   "tipo": "analista", "agente": None},
     # conta genérica já usada por você
-    "analista": {"senha": "1234",       "tipo": "analista", "agente": None},
+    "Breno": {"senha": "Breno13",       "tipo": "analista", "agente": None},
 }
 
 def login_box():
